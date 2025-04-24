e49a348d76173ec3e277d346797452f6a035f67b
KVRTeJ <bb.sssss@inbox.ru>
2025-04-24 21:12:54 +0700

    Созданы файлы классов

---
+++ Moving-figures/area.h
@@ -0,0 +1,12 @@
+#ifndef AREA_H
+#define AREA_H
+
+#include <QWidget>
+
+class Area
+{
+public:
+    Area();
+};
+
+#endif // AREA_H
---
+++ Moving-figures/windows.h
@@ -0,0 +1,12 @@
+#ifndef WINDOWS_H
+#define WINDOWS_H
+
+#include <QWidget>
+
+class Windows
+{
+public:
+    Windows();
+};
+
+#endif // WINDOWS_H
